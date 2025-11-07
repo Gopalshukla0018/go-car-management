07c14b3c0c7743ac8d052d6a3057002f74544125
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-07 17:09:00 +0530

    feat: add validation functions and request structs for Car and Engine models

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Gopalshukla0018/go-car-management
 
 go 1.25.3
 
-require github.com/google/uuid v1.6.0 // indirect
+require github.com/google/uuid v1.6.0
--- models/car.model.go
+++ models/car.model.go
@@ -1,18 +1,118 @@
 package models
 
 import (
+	"errors"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Car struct {
-	ID       uuid.UUID `json:"id"`
-	Name     string    `json:"name"`
-	Year     string    `json:"year"`
-	Brand    string    `json:"brand"`
-	FuelType string    `json:"fuel_type"`
-	Engine   Engine    `json:"engine"`
-	Price    float32   `json:"price"`
-	CreatedAt  time.Time `json:"created_at"`
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Year      string    `json:"year"`
+	Brand     string    `json:"brand"`
+	FuelType  string    `json:"fuel_type"`
+	Engine    Engine    `json:"engine"`
+	Price     float32   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type CarRequest struct {
+	Name     string  `json:"name"`
+	Year     string  `json:"year"`
+	Brand    string  `json:"brand"`
+	FuelType string  `json:"fuel_type"`
+	Engine   Engine  `json:"engine"`
+	Price    float32 `json:"price"`
+}
+
+func validateRequest(carReq CarRequest) error {
+	if err := validateName(carReq.Name); err != nil {
+		return err
+	}
+	if err := validateYear(carReq.Year); err != nil {
+		return err
+	}
+	if err := validateBrand(carReq.Brand); err != nil {
+		return err
+	}
+	if err := validateFuelType(carReq.FuelType); err != nil {
+		return err
+	}
+	if err := validateEngine(carReq.Engine); err != nil {
+		return err
+	}
+	if err := validatePrice(float64(carReq.Price)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("name is required ")
+	}
+	return nil
+}
+
+func validateYear(year string) error {
+	if year == "" {
+		return errors.New("year is Required")
+	}
+	_, err := strconv.Atoi(year)
+	if err != nil {
+		return errors.New("year must be a valid number")
+	}
+	currentYear := time.Now().Year()
+	yearInt, _ := strconv.Atoi(year)
+	if yearInt < 1886 || yearInt > currentYear {
+		return errors.New("year must be between 1886 and current year")
+	}
+	return nil
+
+}
+
+func validateBrand(brand string) error {
+	if brand == "" {
+		return errors.New("brand is required")
+
+	}
+	return nil
+}
+
+func validateFuelType(fuelType string) error {
+	validateFuelType := []string{"Petrol", "Diesel", "Electric", "Hybrid"}
+
+	for _, validTypes := range validateFuelType {
+		if fuelType == validTypes {
+			return nil
+		}
+	}
+	return errors.New("fuel type must be one of the following: Petrol, Diesel, Electric, Hybrid")
+}
+
+func validateEngine(engine Engine) error {
+
+	if engine.EngineID == uuid.Nil {
+		return errors.New("engine id is required")
+	}
+	if engine.Displacement <= 0 {
+		return errors.New("engine displacement must be greater than zero")
+	}
+	if engine.NoOfCylinders <= 0 {
+		return errors.New("number of cylinders must be greater than zero")
+	}
+	if engine.CarRange <= 0 {
+		return errors.New("car range must be greater than zero")
+	}
+	return nil
+}
+func validatePrice(price float64) error {
+	if price <= 0 {
+		return errors.New("price must be greater than zero")
+	}
+	return nil
 }
