57a60f2ef0b95ba2117f6859260669705da9701c
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-08 23:32:48 +0530

    feat: implement engine store methods and structure

--- store/engine/engine.store.go
+++ store/engine/engine.store.go
@@ -1,4 +1,29 @@
 package engine
 
+import (
+	"context"
+	"database/sql"
 
-func Engin
+	"github.com/Gopalshukla0018/go-car-management/models"
+)
+
+type EngineStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EngineStore {
+	return &EngineStore{db: db}
+}
+
+func (e EngineStore) EngineByID(ctx context.Context, id string) (models.Engine, error) {
+
+}
+
+func (e EngineStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
+}
+
+func (e EngineStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
+}
+
+func (e EngineStore) DeleteEngine(ctx context.Context, id string) (models.Engine, error) {
+}
