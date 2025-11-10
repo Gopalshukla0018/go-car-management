80d73f2c39105298ef44bce19ab4fbc2c7dd42d0
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-10 15:34:06 +0530

    feat(driver): Add PostgreSQL database connection setup and .env configuration

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,1 +1,35 @@
 package driver
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+var DB *sql.DB
+
+func ConnectDB() *sql.DB {
+
+	psqlconn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(fmt.Sprintf("Database se connection nahi bana: %s", err.Error()))
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(fmt.Sprintf("Database is not able to ping : %s", err.Error()))
+	}
+
+	fmt.Println("Database connected successfully")
+
+	DB = db
+	return db
+}
--- store/car/car.store.go
+++ store/car/car.store.go
@@ -15,8 +15,18 @@ func new(db *sql.DB) *Store {
 	return &Store{db: db}
 }
 
-func (s Store) GetyByID(ctx context.Context, id string) models.Car {
-
+func (s Store) GetByID(ctx context.Context, id string) (models.Car, error) {
+	var car models.Car 
+	query := `Select c.id, c.name, c.year, c.brand, c.fuel_type, c.engine_id, c.price, c.created_at, c.updated_at, e.id,e.displacement,e.no_of_cyclinders,e.car_range FROM car c LEft join engine e on c.engine_id = e.id WHERE c.id =$1`
+	row := s.db.QueryRowContext(ctx, query, id)
+	err := row.Scan(&car.ID, &car.Name, &car.Year, &car.Brand, &car.FuelType, &car.Engine.EngineID, &car.Price, &car.CreatedAt, &car.UpdatedAt, &car.Engine.EngineID, &car.Engine.Displacement, &car.Engine.NoOfCylinders, &car.Engine.CarRange)
+  if err!=nil{
+	if err==sql.ErrNoRows{
+		return car, nil
+	}
+	return car, err
+  }
+  return car, nil
 }
 
 func (s Store) GetCarBrand(ctx context.Context, brand string, isEngine bool) {
