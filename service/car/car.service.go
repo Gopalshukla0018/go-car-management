e865c6ea43f80c4264054b10bc6bb1e6ab6e5caa
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-19 23:40:07 +0530

    addig service for car

--- service/car/car.service.go
+++ service/car/car.service.go
@@ -1,1 +1,27 @@
 package car
+
+import (
+	"context"
+	"github.com/Gopalshukla0018/go-car-management/models"
+	"github.com/Gopalshukla0018/go-car-management/store"
+)
+
+type Service struct {
+	store store.Car
+}
+
+func New(store store.Car) *Service {
+	return &Service{store: store}
+}
+
+func (s *Service) GetCar(ctx context.Context, id string) (models.Car, error) {
+	return s.store.GetByID(ctx, id)
+}
+
+func (s *Service) CreateCar(ctx context.Context, req models.CarRequest) (models.Car, error) {
+	return s.store.CreateCar(ctx, &req)
+}
+
+func (s *Service) GetCarsByBrand(ctx context.Context, brand string) ([]models.Car, error) {
+	return s.store.GetCarBrand(ctx, brand)
+}
