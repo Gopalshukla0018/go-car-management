6fb6b4b43a85f8264e4ca0593df2022b20d4a332
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-19 23:39:25 +0530

    adding service for engine

--- service/engine/engine.service.go
+++ service/engine/engine.service.go
@@ -1,1 +1,24 @@
 package engine
+
+import (
+	"context"
+	"github.com/Gopalshukla0018/go-car-management/models"
+	"github.com/Gopalshukla0018/go-car-management/store"
+)
+
+type Service struct {
+	store store.Engine
+}
+
+func New(store store.Engine) *Service {
+	return &Service{store: store}
+}
+
+func (s *Service) CreateEngine(ctx context.Context, req models.EngineRequest) (models.Engine, error) {
+	// Models already have validation logic, but you can call it here if exposed
+	return s.store.CreateEngine(ctx, &req)
+}
+
+func (s *Service) GetEngine(ctx context.Context, id string) (models.Engine, error) {
+	return s.store.EngineByID(ctx, id)
+}
