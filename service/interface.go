77b5cd78b404ae0cf979ea177aba5ab2878de3d0
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-19 23:38:44 +0530

    cadding interface

--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"context"
+	"github.com/Gopalshukla0018/go-car-management/models"
+)
+
+type CarService interface {
+	GetCar(ctx context.Context, id string) (models.Car, error)
+	CreateCar(ctx context.Context, carReq models.CarRequest) (models.Car, error)
+	GetCarsByBrand(ctx context.Context, brand string) ([]models.Car, error)
+}
+
+type EngineService interface {
+	CreateEngine(ctx context.Context, engineReq models.EngineRequest) (models.Engine, error)
+	GetEngine(ctx context.Context, id string) (models.Engine, error)
+}
