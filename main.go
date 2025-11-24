bdd4805b0880292a806917d222046c778b4ecbe2
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-24 19:00:00 +0530

    connect local db

--- main.go
+++ main.go
@@ -12,32 +12,38 @@ import (
 	engineService "github.com/Gopalshukla0018/go-car-management/service/engine"
 	carStore "github.com/Gopalshukla0018/go-car-management/store/car"
 	engineStore "github.com/Gopalshukla0018/go-car-management/store/engine"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// 1. Connect to Database	
+
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+
 	db := driver.ConnectDB()
 
-	// 2. Initialize Stores
+
 	eStore := engineStore.New(db)
 	cStore := carStore.New(db)
 
-	// 3. Initialize Services
 	eService := engineService.New(eStore)
 	cService := carService.New(cStore)
 
-	// 4. Initialize Handlers
+\
 	eHandler := engineHandler.New(eService)
 	cHandler := carHandler.New(cService)
 
-	// 5. Register Routes
+
 	http.HandleFunc("POST /engines", eHandler.Create)
-	http.HandleFunc("GET /engines/", eHandler.GetByID) // trailing slash for ID parsing
+	http.HandleFunc("GET /engines/", eHandler.GetByID) 
 	
 	http.HandleFunc("POST /cars", cHandler.Create)
 	http.HandleFunc("GET /cars/", cHandler.GetByID)
 
-	// 6. Start Server
+	
 	fmt.Println("Server starting on port 8000...")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
